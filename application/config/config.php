9d8350f252fdb5492c48dff9b9e33f8996a0b041
TRUCTHANG_COMPUTER <TRUCTHANG_COMPUTER>
2016-12-27 23:13:04 +0700

    clean up public mp3 files

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Ho_Chi_Minh');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:3000/';
+$config['base_url'] = 'http://localhost/e';
 
 /*
 |--------------------------------------------------------------------------
